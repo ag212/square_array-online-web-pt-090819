475f77c702e03d52baa4d3f9f92f46988f437676
Abraham Galva <ag212@icloud.com>
2019-09-30 18:55:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
 square_numbers = [1,2,3]
 counter = 1
 square_array = [array]
-array.each do |numbers|
-  puts "#{numbers}"
+array.each do |(numbers)|
+  puts "#{(numbers)}"
   counter +=1
-end
+  end
 end
